6f4971a7097fe9ceb9c4a6e8b8fff0c6902bfcf7
Yoftahe Abraham <jofab08@gmail.com>
2026-01-10 10:11:58 +0300

    fix: Positions shift for up and down migrations

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,1 +1,6 @@
-DROP TABLE users;
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
